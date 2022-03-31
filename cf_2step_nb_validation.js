dc25d288c4949adeb7cf1be2815b693051bb03ed
Sohail Irfan <73126975+sohailirfan@users.noreply.github.com>
2022-03-31 19:25:20 +0500

    Add files via upload

---
+++ cf_2step_nb_validation.js
@@ -0,0 +1,28 @@
+<script>
+$(document).ready(function() {
+  $('[href="#submit-form-2step"]').attr("href", "#");
+});  
+
+$('[name="email"]').blur(function(){
+  var email = $('[name="email"]').val();
+  var email_regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (email_regex.test(email) == false) {
+    $('[name="email"]').css({"border-color": "rgb(185, 21, 23)", "border-width":"3px", "border-style":"solid", "border-radius":"5px"});   
+  } else {
+    $.ajax({
+      type: 'POST',
+      crossDomain: true,
+      url: 'https://domain.com/validation.php?email='+email, // Update path to PHP
+      success: function(jsondata){ 
+        if (jsondata=="valid") {
+          $('[name="email"]').css({"border-color": "rgb(74, 137, 32)", "border-width":"3px", "border-style":"solid", "border-radius":"5px"});  
+          $('[href="#"]').attr("href", "#submit-form-2step");
+        } else {
+          $('[name="email"]').css({"border-color": "rgb(185, 21, 23)", "border-width":"3px", "border-style":"solid", "border-radius":"5px"});   
+          $('[href="#submit-form-2step"]').attr("href", "#");
+        }
+      }
+    })
+  }
+});
+</script>
